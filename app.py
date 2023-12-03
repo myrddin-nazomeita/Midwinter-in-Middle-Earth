840be79d559fd0a820d42d64ac1893c7cb4208a9
Daniel D <daniel.deutsch@protonmail.com>
2023-12-03 02:28:14 +0100

    Clean and last modifications

--- app.py
+++ app.py
@@ -53,6 +53,7 @@ def add_gift():
         user_list.append({
             'title': request.form['title'],
             'link': request.form['link'],
+            'comment': request.form['comment'],
             'purchased': False,
             'added_by': session['username']
         })
@@ -68,6 +69,46 @@ def purchase_gift(username, gift_index):
             gift_lists[username][int(gift_index)]['purchased'] = True
             save_gift_lists(gift_lists)
     return redirect(url_for('index'))
+
+@app.route('/unpurchase/<username>/<gift_index>')
+def unpurchase_gift(username, gift_index):
+    if 'username' in session and session['username'] != username:
+        gift_lists = load_gift_lists()
+        if username in gift_lists and len(gift_lists[username]) > int(gift_index):
+            gift_lists[username][int(gift_index)]['purchased'] = False
+            save_gift_lists(gift_lists)
+    return redirect(url_for('index'))
+
+@app.route('/remove/<string:user>/<int:index>')
+def remove_gift(user, index):
+    # Ensure that the user is logged in
+    if 'username' in session:
+        try:
+            # Load the current gift lists from the JSON file
+            with open('data/gift_lists.json', 'r') as file:
+                gift_lists = json.load(file)
+
+            # Debugging print
+            print(f"Gift lists loaded: {gift_lists}")
+
+            # Ensure the user and index are valid before attempting to remove the gift
+            if user in gift_lists and index < len(gift_lists[user]):
+                # Remove the gift at the specified index
+                del gift_lists[user][index]
+
+                # Debugging print
+                print(f"Gift at index {index} removed.")
+
+                # Save the updated gift lists back to the JSON file
+                with open('data/gift_lists.json', 'w') as file:
+                    json.dump(gift_lists, file, indent=4)
+        except Exception as e:
+            # Debugging print
+            print(f"An error occurred: {e}")
+# Redirect to the index page or the appropriate page for the gift list
+    return redirect(url_for('index'))
+
+
 
 if __name__ == '__main__':
     # app.run(debug=True)
